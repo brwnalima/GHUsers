89f7d1eff627438f622a25d5bbde351fc940ab7b
brwnalima <bruna.lima@edu.unirio.br>
2023-04-13 18:17:02 -0300

    pastas

---
+++ ghusers/src/components/pages/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+      oi
+    </div>
+  )
+}
+
+export default Search
---
+++ ghusers/src/components/pages/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      oi
+    </div>
+  )
+}
+
+export default User
